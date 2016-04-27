867f13832114e740197d7c09c350dee99446d805
rabbit <rabbit@macRabbit.lan>
2016-04-27 23:15:46 +0800

    2016-04-27 23:17

---
+++ models/runningState.js
@@ -0,0 +1,9 @@
+var mongoose = require('../utils/mongooseUtil');
+
+var Schema = mongoose.Schema;
+var runningStateSchema = new Schema({
+    nameCn: String,
+    nameEn: String
+});
+
+module.exports = mongoose.model('RunningState', runningStateSchema);
